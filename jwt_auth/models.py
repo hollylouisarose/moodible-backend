5273f32fc9739605789de3bc79a4c70c7795a521
hollylouisarose <hlrstratton@gmail.com>
2021-09-22 16:26:39 +0100

    adjusted profile image field

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
-    profile_image = models.CharField(max_length=300)
+    profile_image = models.CharField(max_length=300, blank=True)
