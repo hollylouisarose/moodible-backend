c1d7bc09ac0336c65593805143494d7e8dd6003e
hollylouisarose <hlrstratton@gmail.com>
2021-09-22 14:49:59 +0100

    removed default user profile image

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
-    profile_image = models.CharField(max_length=300, default='https://i.imgur.com/44CkP8U.png')
+    profile_image = models.CharField(max_length=300)
