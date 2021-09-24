e8c03aa5ec0fad4b879c383104a4701218bab657
hollylouisarose <hlrstratton@gmail.com>
2021-09-24 10:31:10 +0100

    corrected trailing slash issue

--- images/urls.py
+++ images/urls.py
@@ -8,7 +8,7 @@ ImageLikeView)
 
 urlpatterns = [
   path('', ImageListView.as_view()),
-  path('<int:pk>', ImageDetailView.as_view()),
+  path('<int:pk>/', ImageDetailView.as_view()),
   path('<int:user_pk>/notes/', NoteCreateView.as_view()),
   path('<int:user_pk>/notes/<int:note_pk>/', NoteDetailView.as_view()),
   path('<int:image_pk>/like/', ImageLikeView.as_view())
